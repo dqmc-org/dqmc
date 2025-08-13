da5f502a678d58b877870ff52263630f6a506489
msousa <msousa@sjtu.edu.cn>
2025-08-13 11:55:10 +0800

    nit make.sh

--- tools/make.sh
+++ tools/make.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 set -o pipefail
 
-
-CXXFLAGS="-O3 -march=native -g -fno-omit-frame-pointer -Wall -Wformat -Wformat=2 -Wimplicit-fallthrough -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -fstrict-flex-arrays=3 -fstack-clash-protection -fstack-protector-strong"
+# . /opt/intel/oneapi/setvars.sh
+CXXFLAGS="-O3 -march=native -DNDEBUG -g -fno-omit-frame-pointer -Wall -Werror -Wformat -Wformat=2 -Wimplicit-fallthrough -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -fstrict-flex-arrays=3 -fstack-clash-protection -fstack-protector-strong"
 cmake -S . -B build -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS="$CXXFLAGS"
 cmake --build build
 ./build/main -c ./example/config.toml -o ./example/output
