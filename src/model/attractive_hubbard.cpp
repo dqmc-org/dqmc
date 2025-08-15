309a9bdb3fdbf569c88d6783d2b9addc8224e72b
msousa <msousa@sjtu.edu.cn>
2025-08-15 11:36:36 +0800

    refactor: simplify exponential calculation in get_update_ratio
    Restructure the exponential factor computation to avoid redundant calculations
    and improve numerical stability by computing exp_factor once and reusing it.

--- src/model/attractive_hubbard.cpp
+++ src/model/attractive_hubbard.cpp
@@ -137,15 +137,12 @@ double AttractiveHubbard::get_update_ratio(Walker& walker, TimeIndex time_index,
   const Eigen::MatrixXd& green_tt_up = walker.GreenttUp();
   const Eigen::MatrixXd& green_tt_dn = walker.GreenttDn();
 
-  const double exp_factor =
-      (std::exp(-2 * this->m_alpha *
-                this->m_bosonic_field(time_index, space_index)) -
-       1);
+  const double exp_factor = std::exp(
+      -2 * this->m_alpha * this->m_bosonic_field(time_index, space_index));
 
-  return std::exp(2 * this->m_alpha *
-                  this->m_bosonic_field(time_index, space_index)) *
-         (1 + (1 - green_tt_up(space_index, space_index)) * exp_factor) *
-         (1 + (1 - green_tt_dn(space_index, space_index)) * exp_factor);
+  return (1 + (1 - green_tt_up(space_index, space_index)) * (exp_factor - 1)) *
+         (1 + (1 - green_tt_dn(space_index, space_index)) * (exp_factor - 1)) /
+         exp_factor;
 }
 
 void AttractiveHubbard::update_greens_function(Walker& walker,
