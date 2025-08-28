bef0e2e4a4f6c5784cb743b36e2a7e34809ba04b
msousa <msousa@sjtu.edu.cn>
2025-08-28 17:44:06 +0800

    fix: token pasting of ',' and __VA_ARGS__ is a GNU extension

--- include/utils/assert.h
+++ include/utils/assert.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <format>
+
 namespace Utils {
 
 void report_failure(const char* file_name, int line, const char* function_name,
@@ -19,10 +21,16 @@ void report_failure(const char* file_name, int line, const char* function_name,
   } while (0)
 #endif
 
+template <typename... Args>
+std::string dqmc_format_error_impl(const char* func, std::string_view format_str, Args&&... args) {
+  return std::vformat(std::string("{}: ") + std::string(format_str),
+                      std::make_format_args(func, std::forward<Args>(args)...));
+}
+
 #if defined(__GNUC__) || defined(__clang__)
-#define dqmc_format_error(format_string, ...) \
-  std::format("{}: " format_string, __PRETTY_FUNCTION__, ##__VA_ARGS__)
+#define dqmc_format_error(format_str, ...) \
+  dqmc_format_error_impl(__PRETTY_FUNCTION__, format_str __VA_OPT__(, ) __VA_ARGS__)
 #else
-#define dqmc_format_error(format_string, ...) \
-  std::format("{}: " format_string, __func__, ##__VA_ARGS__)
+#define dqmc_format_error(format_str, ...) \
+  dqmc_format_error_impl(__func__, format_str __VA_OPT__(, ) __VA_ARGS__)
 #endif
