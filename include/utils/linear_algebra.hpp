af09f03ba7a3b8ac498814a61e5a8fa1a9bf6ddd
msousa <msousa@sjtu.edu.cn>
2025-09-01 12:43:49 +0800

    refactor: eigen_malloc_guard on linear_algebra.hpp

--- include/utils/linear_algebra.hpp
+++ include/utils/linear_algebra.hpp
@@ -6,72 +6,77 @@
 #include <stdexcept>
 
 #include "utils/assert.h"
+#include "utils/eigen_malloc_guard.h"
 
 namespace Utils {
 namespace LinearAlgebra {
-  /**
-   *  SVD decomposition of arbitrary M * N real matrix:
-   *       A  ->  U * S * V^T
-   *  Remind that V is returned in this subroutine, not V transpose.
-   *
-   *  @param mat -> arbitrary `row` * `col` real matrix to be solved.
-   *  @param u -> u matrix of type Eigen::MatrixXd, `row` * `row`.
-   *  @param s -> eigenvalues s of type Eigen::VectorXd, descending sorted.
-   *  @param v -> v matrix of type Eigen::MatrixXd, `col` * `col`.
-   *  @param svd_solver -> A pre-allocated solver object.
-   */
-  inline void dgesvd(const Eigen::MatrixXd& mat, Eigen::MatrixXd& u, Eigen::VectorXd& s,
-                     Eigen::MatrixXd& v, Eigen::JacobiSVD<Eigen::MatrixXd>& svd_solver) {
-    // BUG: Eigen is not compatible with LAPACK dgesvd, make sure to compile
-    // with EIGEN_USE_BLAS and EIGEN_USE_LAPACKE. For more details:
-    // https://eigen.tuxfamily.org/dox-devel/TopicUsingBlasLapack.html
+/**
+ *  SVD decomposition of arbitrary M * N real matrix:
+ *       A  ->  U * S * V^T
+ *  Remind that V is returned in this subroutine, not V transpose.
+ *
+ *  @param mat -> arbitrary `row` * `col` real matrix to be solved.
+ *  @param u -> u matrix of type Eigen::MatrixXd, `row` * `row`.
+ *  @param s -> eigenvalues s of type Eigen::VectorXd, descending sorted.
+ *  @param v -> v matrix of type Eigen::MatrixXd, `col` * `col`.
+ *  @param svd_solver -> A pre-allocated solver object.
+ */
+inline void dgesvd(const Eigen::MatrixXd& mat, Eigen::MatrixXd& u, Eigen::VectorXd& s,
+                   Eigen::MatrixXd& v, Eigen::JacobiSVD<Eigen::MatrixXd>& svd_solver) {
+  // BUG: Eigen is not compatible with LAPACK dgesvd, make sure to compile
+  // with EIGEN_USE_BLAS and EIGEN_USE_LAPACKE. For more details:
+  // https://eigen.tuxfamily.org/dox-devel/TopicUsingBlasLapack.html
+  {
+    EigenMallocGuard<true> alloc_guard;
     svd_solver.compute(mat, Eigen::ComputeFullU | Eigen::ComputeFullV);
-
-    if (svd_solver.info() != Eigen::Success) {
-      throw std::runtime_error(dqmc_format_error("SVD algorithm failed."));
-    }
-
-    u = svd_solver.matrixU();
-    s = svd_solver.singularValues();
-    v = svd_solver.matrixV();
   }
 
-  /**
-   *  Calculate eigenvalues and eigenstates given an arbitrary N * N real
-   * symmetric matrix, A  ->  T^dagger * S * T where T is the
-   * rotation matrix, which is orthogonal; S is a diagonal matrix with
-   * eigenvalues being diagonal elements.
-   *
-   *  @param size -> number of rows/cols.
-   *  @param mat -> arbitrary `size` * `size` real symmetric matrix to be
-   * solved.
-   *  @param s -> diagonal eigen matrix.
-   *  @param t -> rotation matrix, whose columns are corresponding eigenstates.
-   */
-  inline void dsyev(const Eigen::MatrixXd& mat, Eigen::VectorXd& s, Eigen::MatrixXd& t) {
-    DQMC_ASSERT(mat.rows() == mat.cols());
-    DQMC_ASSERT(mat.isApprox(mat.transpose(), 1e-12));
-
-    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(mat);
-
-    if (solver.info() != Eigen::Success) {
-      throw std::runtime_error(dqmc_format_error("Failed to compute eigenvalues."));
-    }
-
-    s = solver.eigenvalues();
-    t = solver.eigenvectors();
+  if (svd_solver.info() != Eigen::Success) {
+    throw std::runtime_error(dqmc_format_error("SVD algorithm failed."));
   }
 
-  inline void solve_X_times_A_eq_B(Eigen::MatrixXd& X_out, const Eigen::MatrixXd& A_mat,
-                                   const Eigen::MatrixXd& B_mat,
-                                   Eigen::ColPivHouseholderQR<Eigen::MatrixXd>& solver) {
-    DQMC_ASSERT(A_mat.rows() == A_mat.cols());
-    DQMC_ASSERT(A_mat.rows() == B_mat.cols());
-    DQMC_ASSERT(X_out.rows() == B_mat.rows());
-    DQMC_ASSERT(X_out.cols() == A_mat.rows());
+  u = svd_solver.matrixU();
+  s = svd_solver.singularValues();
+  v = svd_solver.matrixV();
+}
 
-    solver.compute(A_mat.transpose());
-    X_out.transpose().noalias() = solver.solve(B_mat.transpose());
+/**
+ *  Calculate eigenvalues and eigenstates given an arbitrary N * N real
+ * symmetric matrix, A  ->  T^dagger * S * T where T is the
+ * rotation matrix, which is orthogonal; S is a diagonal matrix with
+ * eigenvalues being diagonal elements.
+ *
+ *  @param size -> number of rows/cols.
+ *  @param mat -> arbitrary `size` * `size` real symmetric matrix to be
+ * solved.
+ *  @param s -> diagonal eigen matrix.
+ *  @param t -> rotation matrix, whose columns are corresponding eigenstates.
+ */
+inline void dsyev(const Eigen::MatrixXd& mat, Eigen::VectorXd& s, Eigen::MatrixXd& t) {
+  DQMC_ASSERT(mat.rows() == mat.cols());
+  DQMC_ASSERT(mat.isApprox(mat.transpose(), 1e-12));
+
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(mat);
+
+  if (solver.info() != Eigen::Success) {
+    throw std::runtime_error(dqmc_format_error("Failed to compute eigenvalues."));
   }
-} // namespace LinearAlgebra
+
+  s = solver.eigenvalues();
+  t = solver.eigenvectors();
+}
+
+inline void solve_X_times_A_eq_B(Eigen::MatrixXd& X_out, const Eigen::MatrixXd& A_mat,
+                                 const Eigen::MatrixXd& B_mat,
+                                 Eigen::ColPivHouseholderQR<Eigen::MatrixXd>& solver) {
+  DQMC_ASSERT(A_mat.rows() == A_mat.cols());
+  DQMC_ASSERT(A_mat.rows() == B_mat.cols());
+  DQMC_ASSERT(X_out.rows() == B_mat.rows());
+  DQMC_ASSERT(X_out.cols() == A_mat.rows());
+
+  EigenMallocGuard<true> alloc_guard;
+  solver.compute(A_mat.transpose());
+  X_out.transpose().noalias() = solver.solve(B_mat.transpose());
+}
+}  // namespace LinearAlgebra
 }  // namespace Utils
